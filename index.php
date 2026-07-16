08c2aa26443597c8f5088db3a1eada357576971c
JustAADD <makilingadrian19@gmail.com>
2026-07-16 10:54:14 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A-Z Peptides PH</title>
+</head>
+
+<body>
+    <h1>Welcome to A-Z Peptides PH</h1>
+    <p>This is the homepage of A-Z Peptides PH.</p>
+
+</body>
+
+</html>
