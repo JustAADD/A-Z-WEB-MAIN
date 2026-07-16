7978b347692046673aa31fe5dc2c2e3aea00e51f
JustAADD <makilingadrian19@gmail.com>
2026-07-16 11:06:18 +0800

    initial commit

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>A-Z Peptides PH</title>
+    <link href="./output.css" rel="stylesheet">
 </head>
 
 <body>
     <h1>Welcome to A-Z Peptides PH</h1>
     <p>This is the homepage of A-Z Peptides PH.</p>
-
 </body>
 
 </html>
