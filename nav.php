397462bf86015fb326665ca31e6cbcc5bd7c7b62
JustAADD <makilingadrian19@gmail.com>
2026-07-20 10:16:57 +0800

    navbar

--- nav.php
+++ nav.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./src/main.css" rel="stylesheet">
     <link href="./src/output.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
@@ -15,16 +18,20 @@
         </div>
 
         <ul id="menuList">
-            <li><a href="./index.php">Home</a></li>
-            <li><a href="./about.php">About</a></li>
-            <li><a href="./shop.php">Shop</a></li>
-            <li><a href="./contact.php">Contact</a></li>
+            <li class="font-poppins"><a href="./index.php">Home</a></li>
+            <li class="font-poppins"><a href="./about.php">Products</a></li>
+            <li class="font-poppins"><a href="./shop.php">Contact</a></li>
+            <li class="font-poppins"><a href="./contact.php">About us</a></li>
+
+
+            <li class="soc-links gap-1">
+                <i class="fa-brands fa-tiktok me-1"></i>
+                <i class="fa-brands fa-square-instagram"></i>
+            </li>
         </ul>
 
-        <div class="soc-links">
-            <i class="fa-brands fa-square-facebook"></i>
-            <i class="fa-brands fa-shopify"></i>
-        </div>
+
+
         <div class="menu-icon">
             <i class="fa-solid fa-bars-staggered" onclick="toggleMenu()"></i>
         </div>
@@ -32,25 +39,25 @@
 
 
     <script>
-        let menuList = document.getElementById("menuList")
+        let menuList = document.getElementById("menuList");
+
         menuList.style.maxHeight = "0px";
 
         function toggleMenu() {
-            if (menuList.style.maxHeight === "0px" || menuList.style.maxHeight === "") {
+            const isClosed = menuList.style.maxHeight === "0px" || menuList.style.maxHeight === "";
+
+            if (isClosed) {
                 // OPEN
-                menuList.style.marginTop = "0px";
                 menuList.style.maxHeight = "500px";
                 menuList.style.padding = "0px 0";
+
             } else {
                 // CLOSE
-                menuList.style.marginTop = "0px";
                 menuList.style.maxHeight = "0px";
                 menuList.style.padding = "0px";
             }
         }
     </script>
-
-
 
 </body>
 
